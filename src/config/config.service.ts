2f31de578f10c8171b7ee13756eba1524edf520d
Donald Whitely <dswhitely1@gmail.com>
2020-04-14 08:58:28 -0400

    Initial Commit, Config Service

---
+++ src/config/config.service.ts
@@ -0,0 +1,62 @@
+import {TypeOrmModuleOptions} from '@nestjs/typeorm';
+require('dotenv').config();
+
+class ConfigService {
+  constructor(private env: {[key: string]: string | undefined}) {
+  }
+
+  private getValue(key: string, throwOnMissing = true): string {
+    const value = this.env[key];
+    if (!value && throwOnMissing) {
+      throw new Error(`Missing Environment Variable`)
+    }
+    return value;
+  }
+
+  public ensureValues(keys: string[]) {
+    keys.forEach(key => this.getValue(key, true));
+    return this;
+  }
+
+  public getSecret(): string {
+    return this.getValue('JWT_SECRET');
+  }
+
+  public getPort(): number {
+    return Number(this.getValue('PORT'));
+  }
+
+  public isProduction(): boolean {
+    return this.getValue('NODE_ENV') === 'production'
+  }
+
+  public getTypeOrmConfig(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: this.getValue('DB_HOST'),
+      port: Number(this.getValue('DB_PORT')),
+      username: this.getValue('DB_USER'),
+      password: this.getValue('DB_PASS'),
+      database: this.getValue('DB_NAME'),
+
+      entities: ['**/*.entity{.ts,.js}'],
+      migrationsTableName: 'migrations',
+
+      migrations: ['src/migrations/*.ts'],
+      cli: {
+        migrationsDir: 'src/migrations'
+      },
+      ssl: this.isProduction()
+    }
+  }
+}
+
+export const configService = new ConfigService(process.env).ensureValues([
+  'JWT_SECRET',
+  'PORT',
+  'DB_HOST',
+  'DB_PORT',
+  'DB_USER',
+  'DB_PASS',
+  'DB_NAME'
+]);
